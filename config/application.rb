cb27c7fc93934404826a921f1b9c493a55f43df6
ysmr0001 <adds.3.luvmid@gmail.com>
2020-01-30 14:12:01 +0900

    メッセージの自動スクロール機能追加

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
