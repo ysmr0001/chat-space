1ee46612cc63b9bec9a56871eb18ddc1861f3f63
ysmr0001 <adds.3.luvmid@gmail.com>
2020-02-05 16:48:40 +0900

    コメント消去

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,6 @@ $(function(){
 
   var buildHTML = function(message) {
     if (message.content && message.image) {
-      //data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -20,7 +19,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -37,7 +35,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
         `<div class="upper-message">` +
           `<div class="upper-message__user-name">` +
@@ -53,7 +50,7 @@ $(function(){
       `</div>`
     };
     return html;
-  };
+  }
 
   $('#new_message').on('submit', function(e){
   e.preventDefault();
@@ -82,26 +79,19 @@ $(function(){
 
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       }
